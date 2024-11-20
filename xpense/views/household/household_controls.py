2583ac4572912f7e1dccb05aa0b9faf14b3d0655
Cristian <cristian.ciortea@syneto.eu>
2024-11-20 20:01:14 +0200

    Add feature to convert with factor the transactions to aggregation level.

--- xpense/views/household/household_controls.py
+++ xpense/views/household/household_controls.py
@@ -1,22 +1,18 @@
 import calendar
 import datetime
-from calendar import month
-from collections import defaultdict
-from datetime import timedelta
-from typing import List, Optional, Callable, Tuple, Dict
+from typing import List, Optional, Callable, Tuple
 
 import flet as ft
-from anyio.abc import value
-from dateutil.relativedelta import relativedelta
 from flet_core import PopupMenuPosition, ControlEvent
 
 from xpense.database.repository_container import RepositoryContainer
-from xpense.types import DataAggregation, Transaction, Currency, TransactionType, TransactionOperations, \
+from xpense.types import Transaction, Currency, TransactionType, TransactionOperations, \
     DataAggregation
 from xpense.utilities.common import round_to_two_decimals, human_readable_datetime
+from xpense.views.household.balance_calculator import BalanceCalculator
+from xpense.views.household.transaction_category_button import DEFAULT_EXPENSE_CATEGORIES_WITH_ICONS
 from xpense.views.household.transaction_fetcher import TransactionFetcher
 from xpense.views.household.transaction_view import get_transaction_view, TransactionPipe, CURRENCY_TO_ICONS
-from xpense.views.household.transaction_category_button import DEFAULT_EXPENSE_CATEGORIES_WITH_ICONS
 
 
 def get_main_column() -> ft.Column:
@@ -41,72 +37,12 @@ def get_main_column() -> ft.Column:
     )
 
 
-class OverviewSectionCalculator:
-    def __init__(self, repository_container: RepositoryContainer, current_datetime: datetime.datetime):
-        self._transaction_fetcher = TransactionFetcher(repository_container, current_datetime)
-
-    def get_total_expenses(self, aggregation: Optional[DataAggregation] = DataAggregation.MONTHLY) -> float:
-        expense_transactions = self._transaction_fetcher.get_expense_transactions(aggregation)
-        return sum(float(t.amount) for t in expense_transactions)
-
-    def get_total_income(self) -> float:
-        income_transactions = self._transaction_fetcher.get_income_transactions()
-        return sum(float(t.amount) for t in income_transactions)
-
-    def get_total_allocations(self) -> float:
-        allocation_transactions = self._transaction_fetcher.get_allocation_transactions()
-        return sum(float(t.amount) for t in allocation_transactions)
-
-    def get_total_unallocated_expenses(self, aggregation: Optional[DataAggregation] = DataAggregation.MONTHLY) -> float:
-        """
-        Calculate unallocated expenses based on the following rules:
-        - For each category:
-            - If there is an allocation:
-                - If allocation is negative, the absolute value is considered unallocated expense.
-            - If there is no allocation, all expenses for that category are unallocated.
-        """
-        unallocated_expenses = 0.0
-        allocations: List[Transaction] = self._transaction_fetcher.get_allocation_transactions()
-        expenses: List[Transaction] = self._transaction_fetcher.get_expense_transactions(aggregation)
-
-        # Sum allocations and expenses per category.
-        allocation_per_category: Dict[str, float] = defaultdict(float)
-        for alloc in allocations:
-            allocation_per_category[alloc.category] += float(alloc.amount)
-
-        expense_per_category: Dict[str, float] = defaultdict(float)
-        for exp in expenses:
-            expense_per_category[exp.category] += float(exp.amount)
-
-        # Set of all categories involved
-        transaction_categories = set(allocation_per_category.keys()).union(set(expense_per_category.keys()))
-
-        for category in transaction_categories:
-            allocation = allocation_per_category.get(category, 0.0)
-            expense = expense_per_category.get(category, 0.0)
-
-            if allocation < expense:
-                # Expenses exceed allocation; unallocated expense is the difference.
-                unallocated_expenses += (expense - allocation)
-            elif allocation == 0:
-                # No allocation for this category; entire expense is unallocated.
-                unallocated_expenses += expense
-
-        return unallocated_expenses
-
-    def get_total_balance(self, aggregation: Optional[DataAggregation] = DataAggregation.MONTHLY):
-        total_income = self.get_total_income()
-        total_allocations = self.get_total_allocations()
-        total_unallocated_expenses = self.get_total_unallocated_expenses(aggregation)
-        return total_income - total_allocations - total_unallocated_expenses
-
-
 class OverviewSection:
     def __init__(
             self, repository_container: RepositoryContainer, current_datetime: datetime.datetime,
             data_aggregation: DataAggregation
     ):
-        self._calculator = OverviewSectionCalculator(repository_container, current_datetime)
+        self._calculator = BalanceCalculator(repository_container, current_datetime)
         self._data_aggregation = data_aggregation
 
         self._expenses_amount = None
